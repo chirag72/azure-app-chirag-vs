f990b2109d96619615d8c1e79a8df737f3d820b6
chirag72 <chirag.patel@fsastore.com>
2023-08-24 15:24:20 -0400

    Added EF Core Azure SQL Database Connection

--- azure-app-chirag-vs/Program.cs
+++ azure-app-chirag-vs/Program.cs
@@ -1,8 +1,17 @@
+using azure_app_chirag_vs.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString
+    ("AzureSqlConnection");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer
+    (connectionString));
+
 builder.Services.AddRazorPages();
-builder.Services.AddApplicationInsightsTelemetry(builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]);
+builder.Services.AddApplicationInsightsTelemetry(builder.Configuration
+    ["APPLICATIONINSIGHTS_CONNECTION_STRING"]);
 
 var app = builder.Build();
 
